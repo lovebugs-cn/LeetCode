32015291fde0af592d7ecff749a2aa518e44c487
Josiah <1319164238@qq.com>
2017-12-19 12:19:34 +0800

    220 Contains DuplicationⅢ

--- ContainsNearbyAlmostDuplicate.java
+++ ContainsNearbyAlmostDuplicate.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class ContainsNearbyAlmostDuplicate {
@@ -22,10 +24,30 @@ public class ContainsNearbyAlmostDuplicate {
 
     }
 
+    public static boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t){
+
+        if(k<1||t<0) return false;
+
+        TreeSet<Long> set = new TreeSet<Long>();
+        for(int i=0;i<nums.length;i++){
+            if(i>k)
+                set.remove((long)nums[i-k-1]);
+
+            Set<Long> resSet = set.subSet((long)nums[i] - t, (long)nums[i] + t + 1);
+
+            if(!resSet.isEmpty())
+                return true;
+            set.add((long)nums[i]);
+
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,0,1,1};
         int k = 1;
         int t = 0;
-        System.out.println(containsNearbyAlmostDuplicate(nums, k, t));
+        System.out.println(containsNearbyAlmostDuplicate2(nums, k, t));
     }
 }
